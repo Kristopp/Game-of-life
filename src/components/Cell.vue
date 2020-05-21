ac331df6f9aee7196477f0a575b67c406244d6b6
kristo palu <kristo.palu89@gmail.com>
2020-05-21 21:08:39 +0300

    First commit

---
+++ src/components/Cell.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="firstCell-class"></div>
+</template>
+    <style src="@/desgin/cell.css" lang="css"></style>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Cell extends Vue {}
+</script>
---
+++ src/components/World.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid" style="worldCss">
+    <FirstCell></FirstCell>
+  </div>
+</template>
+<style src="@/desgin/world.css" lang="css"></style>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import FirstCell from "@/components/Cell.vue";
+@Component({
+  components: {
+    FirstCell
+  }
+})
+export default class World extends Vue {
+  gridValues = {
+    colums: 0,
+    rows: 0
+  };
+  private createGrid(): void {
+    console.log();
+  }
+  mounted() {
+    this.createGrid();
+  }
+}
+</script>
