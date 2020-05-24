a2608b7910525b83e99c7841be95ca5f9f42d256
kristo palu <kristo.palu89@gmail.com>
2020-05-24 22:15:35 +0300

    how to manipulate DOM in vue and same basic typescript

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="firstCell-class"></div>
+  <div class="livingCell"></div>
 </template>
-    <style src="@/desgin/cell.css" lang="css"></style>
+<style src="@/desgin/livingCell.css" lang="css"></style>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class Cell extends Vue {}
+export default class Cell extends Vue {
+  
+}
 </script>
--- src/components/World.vue
+++ src/components/World.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="grid" style="worldCss">
-    <FirstCell></FirstCell>
+  <!-- main container where the magic begins -->
+  <div class="grid-container" id="grid-container-id">
+    <LivingCell v-if="cellState"></LivingCell>
+    <button v-on:click="renderLivingCell" style="margin: 1rem">click</button>
   </div>
 </template>
 <style src="@/desgin/world.css" lang="css"></style>
+<style src="@/desgin/gridCell.css" lang="css"></style>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import FirstCell from "@/components/Cell.vue";
+import LivingCell from "@/components/Cell.vue";
 @Component({
   components: {
-    FirstCell
+    LivingCell
   }
 })
 export default class World extends Vue {
-  gridValues = {
-    colums: 0,
-    rows: 0
-  };
-  private createGrid(): void {
-    console.log();
+  public cellState = false;
+  _i = 0;
+  
+  /* function to create dynamic grid cells */
+  /**loop for dynamic grid */
+  createGrid() {
+    for (let _i= this.i;_i < 10; this._i++) {
+      const getGridCell = `<div class="main-grid" id=${this._i} v-for="item in this.gridId" v-bind:key="item.id">`;
+      const element: any = document.querySelector(".grid-container").insertAdjacentHTML('beforeend', getGridCell);
+      console.log(element);
+    }
+  }
+  renderLivingCell() {
+    this.cellState = true;
   }
   mounted() {
     this.createGrid();
